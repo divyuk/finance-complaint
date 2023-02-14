8dc5a64a0375775f692cf45aa069255cb8934789
divyuk <div14kes@gmail.com>
2023-02-14 16:32:04 +0530

    training part indata ingestion  and artifact for data ingestion

--- finance_complaint/constant/prediction_pipeline_config/file_config.py
+++
@@ -1,8 +0,0 @@
-import os
-from datetime import datetime
-from finance_complaint.constant import TIMESTAMP
-
-INPUT_DIR_NAME = "input"
-PREDICTION_DIR_NAME = "prediction"
-FAILED_DIR_NAME = "failed"
-REGION_NAME = "ap-south-1"
