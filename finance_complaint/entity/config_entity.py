247bd62af120c2ea75249df15142a6430219d849
divyuk <div14kes@gmail.com>
2023-02-14 15:52:05 +0530

    Configuration for data ingestion

---
+++ finance_complaint/entity/config_entity.py
@@ -0,0 +1,16 @@
+from collections import namedtuple
+from finance_complaint.constant.prediction_pipeline_config.file_config import ARCHIVE_DIR, INPUT_DIR, FAILED_DIR, \
+    PREDICTION_DIR, REGION_NAME
+
+TrainingPipelineConfig = namedtuple("PipelineConfig", ["pipeline_name", "artifact_dir"])
+
+DataIngestionConfig = namedtuple("DataIngestionConfig", ["from_date",
+                                                         "to_date",
+                                                         "data_ingestion_dir",
+                                                         "download_dir",
+                                                         "file_name",
+                                                         "feature_store_dir",
+                                                         "failed_dir",
+                                                         "metadata_file_path",
+                                                         "datasource_url"
+                                                         ])
